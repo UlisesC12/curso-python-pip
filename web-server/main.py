6de1c7a2f7fc24050359230cc90f6e1193d1f1e7
Ulises Candela <ulisescandela99@gmail.com>
2024-01-17 23:15:08 -0600

    Backend practice: Web Server created with python and FastAPI

--- web-server/main.py
+++ web-server/main.py
@@ -1,5 +1,19 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+  return [1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+  return """
+  <h1>Titulo de pagina</h1>
+  <p>Parrafo de pagina</p>
+"""
 def run():
   store.get_categories()
   
